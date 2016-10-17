8cb05b72d79d1806bd26272d1433d85d6ca9350b
Kyungyong Lee <leeky@kookmin.ac.kr>
2016-10-17 21:44:30 +0900

    adding a file to fetch homework2 info

---
+++ fetch-submission.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+  echo "GIT URL: $line"
+  if [ -z ${line} ]; then 
+    echo "empty URL. continue"
+    continue 
+  fi
+  git clone $line
+  cd oss-2016-homework
+  git log -2
+  git branch
+  echo "XXXXXXX - should print ID"
+  cat *
+  git checkout homework-2
+  git log -2
+  echo "YYYYYYY - should have oss-homework file"
+  ls -l
+  cd ../
+  rm -rf oss-2016-homework
+  echo -e "ZZZZZZZZZZZZ\nEnd of a student\n\n\n"
+done < "$1"
