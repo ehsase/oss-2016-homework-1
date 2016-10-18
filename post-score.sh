1a2395d27ab0ea9fa5fc85a660a232c42013719b
Kyungyong Lee <leeky@kookmin.ac.kr>
2016-10-18 22:28:03 +0900

    adding a script to post score

---
+++ post-score.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+  url=$(echo "$line" | awk '{print $1}')
+  score=$(echo "$line" | awk '{print $2}')
+  echo $url
+  echo $score
+  git clone $url
+  cd oss-2016-homework
+  targetrepo="homework-2"
+  git checkout $targetrepo
+  hw2exist=$(echo $?)
+  if [ "$hw2exist" != "0" ]; then
+    git checkout -b hw2-score
+    targetrepo="hw2-score"
+  fi
+  touch $score
+  git add $score
+  git commit -m "Your homework-2 score"
+  git push origin $targetrepo
+  cd ..
+  rm -rf oss-2016-homework
+done < "$1"
